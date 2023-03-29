2b7b98e2a538cb72fcb1eb9e2719dfe942b6476f
Jaroslaw Szczepaniak <jarek.szczepaniak@gmail.com>
2023-03-29 22:56:30 +0200

    Test implementation of vehicle-related classess

--- my_fancy_vehicles.py
+++ my_fancy_vehicles.py
@@ -2,3 +2,7 @@ from gear import vehicles
 
 some_bike = vehicles.Bicycle()
 print(some_bike.fuel_type)
+some_bike.go_forward()
+some_bike.go_backward()
+some_bike.add_fuel("sausage", 300)
+some_bike.add_fuel("gas", 2)
