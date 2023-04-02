ab67cf92163558d9128415a1a295e1b071b86a46
Jaroslaw Szczepaniak <jarek.szczepaniak@gmail.com>
2023-04-02 21:09:25 +0200

    Changes in Energy Source

--- gear/energy_sources.py
+++ gear/energy_sources.py
@@ -2,7 +2,7 @@ from gear.fuel_units import fuel_units
 from abc import ABC
 
 class Generic_energy_source(ABC):
-    def __init__(self, supported_fuel) -> None:
+    def __init__(self, supported_fuel: str) -> None:
         if supported_fuel in fuel_units:
             self.supported_fuel = supported_fuel
             self.fuel_level = 0.0
@@ -10,7 +10,19 @@ class Generic_energy_source(ABC):
             raise ValueError
             
     def add_fuel(self, value: float, unit: str) -> float:
-        if unit in fuel_units.items():
+        """AI is creating summary for add_fuel
+
+        Args:
+            value (float): [description]
+            unit (str): [description]
+
+        Raises:
+            ValueError: [description]
+
+        Returns:
+            float: [description]
+        """
+        if unit in fuel_units:
             self.fuel_level += value
         else:
             raise ValueError
